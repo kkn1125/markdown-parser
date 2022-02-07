7d8677338b0836a6f7be15705f5de6ab534d6808
kkn1125 <xpfjrnseks1@naver.com>
2022-02-07 17:05:55 +0900

    update list, blockquote

--- markdown/test.js
+++ markdown/test.js
@@ -13,26 +13,41 @@ export const test = `
 
 > test
     > test
+        > test
+    > test
+    1. qweqwe
+    2. qweqweqweqwe
 
 > test
 
 ![test](./weqr.jpg 'qweqwe')
 
-1. qeqweqwe
-2. fasdfasdf
-3. zxcvxcvxcbv
-    1. qeqweqwe
-    2. fasdfasdf
-    3. zxcvxcvxcbv
-        1. qeqweqwe
-        2. fasdfasdf
-        3. zxcvxcvxcbv
-    1. qeqweqwe
-    2. fasdfasdf
-    3. zxcvxcvxcbv
-1. qeqweqwe
-2. fasdfasdf
-3. zxcvxcvxcbv
+1. 첫 번째 문단
+2. 첫 번째 문단
+3. 첫 번째 문단
+    1. 두 번째 문단
+    2. 두 번째 문단
+    3. 두 번째 문단
+        1. 체크박스 [x]
+        2. 체크박스 []
+            3. 체크박스 [x]
+                - 중간 변환
+                    1. qweqwe
+    1. 다시 변환
+    2. 다시 변환
+    3. 다시 변환
+
+1. 마지막 문단
+2. 마지막 문단
+3. 마지막 문단
+    1. 마지막 문단
+    2. 마지막 문단
+    3. 마지막 문단
+        - qweqwe
+        - qweqwe
+
+- qweqwe
+- qweqwe
 
 1. qeqweqwe
 2. fasdfasdf
@@ -50,15 +65,22 @@ export const test = `
     - werqwer
     - sdfsdfg
     - xcvbxcvb
-- werqwer
-- sdfsdfg
-- xcvbxcvb
 
 - werqwer
 - sdfsdfg
 - xcvbxcvb
 
+<div>qweqwe</div>
+
+:raw
+- werqwer
+- sdfsdfg
+- xcvbxcvb
+:rawend
+
+:raw
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque libero ducimus quos tenetur deleniti doloremque molestiae necessitatibus dignissimos exercitationem, quod, temporibus eaque quam inventore harum architecto autem veritatis, ea sequi.
+:rawend
 
 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat molestias dolor in? A laborum aspernatur cupiditate, recusandae sunt, minus officia facilis, autem perferendis voluptas deleniti incidunt sit enim. Expedita, neque.
 `
--- mdParser.js
+++ mdParser.js
@@ -1,8 +1,9 @@
-// fetch('./markdown/test.md').then(data => {
-//     return data.text();
-// }).then(data => {
-//     Markdown.parse(data);
-// })
+/**
+ * @author kimson
+ * @since 2022. 02. 07
+ * @license MITLicense
+ * @description 아직 정리 중 입니다.  미비한 부분이 많습니다.
+ */
 
 import {test} from './markdown/test.js'
 
@@ -18,13 +19,15 @@ const Markdown = (function () {
     function Model() {
         const INDENT = 4;
         let views;
+        let options;
         let md;
         let block;
         let temp;
         let convertedHTML = [];
 
-        this.init = function (view) {
+        this.init = function (view, option) {
             views = view;
+            options = option;
             md = views.getMd();
             this.parse();
             views.renderView(temp, convertedHTML.join(''));
@@ -32,12 +35,12 @@ const Markdown = (function () {
 
         this.parse = function () {
             this.readBlockUnit();
+            this.raw();
             this.heading();
-            this.unOrderedList();
-            this.orderedList();
-            this.blockquote();
+            this.blockListify();
             this.images();
             this.anchors();
+            this.paragraphs();
         }
 
         this.readBlockUnit = function (){
@@ -45,134 +48,204 @@ const Markdown = (function () {
             temp = [...block];
         }
 
+        this.raw = function (){
+            block.forEach((line, id)=>{
+                if(line.match(/:raw([\s\S]+):rawend/gm)){
+                    convertedHTML[id] = `<p>${line.match(/:raw([\s\S]+):rawend/)[1]}</p>`;
+                    block[id] = '';
+                }
+            });
+        }
+
         this.heading = function (){
             block.forEach((line, id)=>{
                 if(line.match(/(\#+)/gm)){
                     convertedHTML[id] = line.replace(/[\s\n]*(\#*)(.+)/gm, (a,$1,$2)=>{
                         let count = $1.split('').length;
-                        return `<h${count}>${$2.replace(/[\s]*/g, '')}</h${count}>`
+                        return `<h${count}${options.h?` class="h${count}"`:''}>${$2.replace(/[\s]*/g, '')}</h${count}>`
                     });
                     block[id] = '';
                 }
             });
         }
 
-        this.unOrderedList = function (){
+        this.blockListify = function (){
             let indent = 0, before = -1;
             let count = 0;
+            let isDouble = 1;
+            let tag = '';
+            let array = [];
+
             block.forEach((line, id)=>{
-                if(line.match(/\s*\-/gm)){
+                if(line.match(/\s*\>\s/gm) || line.match(/\s*\-/gm) || line.match(/\s*[0-9]+\./gm)){
                     convertedHTML[id] = line.split(/\n/gm).filter(x=>x!='').map(x=>{
                         let temp = '';
                         let space = x.match(/(^\s*)/gm)[0];
-                        indent = space.length;
                         
+                        indent = space.length;
+
+                        if(indent>0 && before==-1){
+                            isDouble++;
+                        } else {
+                            isDouble = 1;
+                        }
+                        
+                        if(x.match(/^\s*\-/gm)){
+                            tag = 'ul';
+                            array.push(tag);
+                            // tag = '';
+                        }
+                        if(x.match(/^\s*[0-9]+\./gm)){
+                            tag = 'ol';
+                            array.push(tag);
+                            // tag = '';
+                        }
+                        if(x.match(/^\s*\>\s/gm)){
+                            tag = 'blockquote';
+                            array.push(tag);
+                            // tag = '';
+                        }
+
                         if(indent > before){
                             count++;
-                            temp += `<ul>`;
+                            temp += `<${array[array.length-1]} ${options[tag]?`class="${options[tag]}"`:''}>`.repeat(isDouble);
                         } else if(indent == before) {
 
                         } else {
-                            temp += '</ul>';
+                            let gap = parseInt(before/indent);
+                            for(let i=0;i<gap;i++){
+                                temp += `</${array.pop()}>`;
+                            }
+                            temp += `<${array[array.length-1]} ${options[tag]?`class="${options[tag]}"`:''}>`;
                             count--;
                         }
 
-                        if(indent<before){
-                            temp += `</ul><ul>`
-                        }
                         
-                        temp += `<li>${x.replace(/\s*\-\s*/gm, '')}</li>`
+                        if(array[array.length-1] == 'blockquote'){
+                            temp += `${this.checkbox(x.replace(/\s*\>\s/gm, ''))}`;
+                        } else {
+                            temp += `<li ${options.li?`class="${options.li}"`:''}>${this.checkbox(x.replace(/\s*[0-9]\.\s*/gm, '').replace(/\s*\-\s*/gm, ''))}</li>`;
+                        }
 
                         before = indent;
                         return temp;
                     }).join('\n');
-                    convertedHTML[id] += '</ul>'.repeat(count);
+                    while(array.length>0){
+                        convertedHTML[id] += `</${array.pop()}>`;
+                    }
+                    block[id] = '';
                 }
                 indent = 0;
                 before = -1;
                 count = 0;
+                array = [];
             });
         }
 
-        this.orderedList = function (){
-            let indent = 0, before = -1;
-            let count = 0;
-            block.forEach((line, id)=>{
-                if(line.match(/\s*[0-9]+\./gm)){
-                    convertedHTML[id] = line.split(/\n/gm).map(x=>{
-                        let temp = '';
-                        let space = x.match(/(^\s*)/gm)[0];
-                        indent = space.length;
+        // this.orderedList = function (){
+        //     let indent = 0, before = -1;
+        //     let count = 0;
+        //     let isDouble = 1;
+        //     block.forEach((line, id)=>{
+        //         if(line.match(/\s*[0-9]+\./gm)){
+        //             convertedHTML[id] = line.split(/\n/gm).map(x=>{
+        //                 let temp = '';
+        //                 let space = x.match(/(^\s*)/gm)[0];
                         
-                        if(indent > before){
-                            count++;
-                            temp += `<ol>`;
-                        } else if(indent == before) {
+        //                 indent = space.length;
 
-                        } else {
-                            temp += '</ol>';
-                            count--;
-                        }
-
-                        if(indent<before){
-                            temp += `</ol><ol>`;
-                        }
+        //                 if(indent>0 && before==-1){
+        //                     isDouble++;
+        //                 } else {
+        //                     isDouble = 1;
+        //                 }
                         
-                        temp += `<li>${x.replace(/\s*[0-9]\.\s*/gm, '')}</li>`;
+        //                 if(indent > before){
+        //                     count++;
+        //                     temp += `<ol>`.repeat(isDouble);
+        //                 } else if(indent == before) {
 
-                        before = indent;
-                        return temp;
-                    }).join('\n');
-                    convertedHTML[id] += '</ol>'.repeat(count);
-                }
-                indent = 0;
-                before = -1;
-                count = 0;
-            });
+        //                 } else {
+        //                     temp += '</ol>';
+        //                     count--;
+        //                 }
+
+        //                 if(indent<before){
+        //                     temp += `</ol><ol>`;
+        //                 }
+                        
+        //                 temp += `<li>${this.checkbox(x.replace(/\s*[0-9]\.\s*/gm, ''))}</li>`;
+
+        //                 before = indent;
+        //                 isDouble = 1;
+        //                 return temp;
+        //             }).join('\n');
+        //             convertedHTML[id] += '</ol>'.repeat(count+1);
+        //             block[id] = '';
+        //         }
+        //         indent = 0;
+        //         before = -1;
+        //         count = 0;
+        //     });
+        // }
+
+        this.checkbox = function(str){
+            let ox = str.match(/\[\s?(x?)\s?\]/);
+            if(ox) return str.replace(/\[\s?(x?)\s?\]/, `<input disabled type="checkbox"${ox[1]?` checked="true"`:``}>`);
+            else return str;
         }
 
-        this.blockquote = function (){
-            let indent = 0, before = -1;
-            let count = 0;
-            block.forEach((line, id)=>{
-                if(line.match(/\s*\>\s/gm)){
-                    convertedHTML[id] = line.split(/\n/gm).map(x=>{
-                        let temp = '';
-                        let space = x.match(/(^\s*)/gm)[0];
-                        indent = space.length;
+        // this.blockquote = function (){
+        //     let indent = 0, before = -1;
+        //     let count = 0;
+        //     let isDouble = 1;
+        //     block.forEach((line, id)=>{
+        //         if(line.match(/\s*\>\s/gm)){
+        //             convertedHTML[id] = line.split(/\n/gm).map(x=>{
+        //                 let temp = '';
+        //                 let space = x.match(/(^\s*)/gm)[0];
+        //                 indent = space.length;
+
+        //                 if(indent>0 && before==-1){
+        //                     isDouble++;
+        //                 } else {
+        //                     isDouble = 1;
+        //                 }
                         
-                        if(indent > before){
-                            count++;
-                            temp += `<blockquote>`;
-                        } else if(indent == before) {
+        //                 if(indent > before){
+        //                     count++;
+        //                     temp += `<blockquote>`.repeat(isDouble);
+        //                 } else if(indent == before) {
 
-                        } else {
-                            temp += '</blockquote>';
-                            count--;
-                        }
+        //                 } else {
+        //                     temp += '</blockquote>';
+        //                     count--;
+        //                 }
 
-                        if(indent<before){
-                            temp += `</blockquote><blockquote>`;
-                        }
+        //                 if(indent<before){
+        //                     temp += `</blockquote><blockquote>`;
+        //                 }
                         
-                        temp += `${x.replace(/\s*\>\s/gm, '')}`;
+        //                 temp += `${x.replace(/\s*\>\s/gm, '')}`;
 
-                        before = indent;
-                        return temp;
-                    }).join('\n');
-                    convertedHTML[id] += '</blockquote>'.repeat(count);
-                }
-                indent = 0;
-                before = -1;
-                count = 0;
-            });
-        }
+        //                 before = indent;
+        //                 return temp;
+        //             }).join('\n');
+        //             convertedHTML[id] += '</blockquote>'.repeat(count+1);
+        //             block[id] = '';
+        //         }
+        //         indent = 0;
+        //         before = -1;
+        //         count = 0;
+        //     });
+        // }
 
         this.images = function (){
             block.forEach((line, id)=>{
                 if(line.match(/^\!\[/gm)){
                     const [a,$1,$2,$3] = line.match(/\!\[(.+)\]\(([A-z0-9\.\:\@\/\-\_ㄱ-힣]+)(\s.+)?\)/);
-                    convertedHTML[id] = `<img src="${$2}" alt="${$1}"${$3?` title="${$3.replace(/[\'\"]+/gm,'').trim()}"`:''}>`
+                    convertedHTML[id] = `<img src="${$2}" alt="${$1}"${$3?` title="${$3.replace(/[\'\"]+/gm,'').trim()}"`:''}>`;
+                    block[id] = '';
                 }
             });
         }
@@ -181,7 +254,17 @@ const Markdown = (function () {
             block.forEach((line, id)=>{
                 if(line.match(/^\!\[/gm)){
                     const [a,$1,$2,$3] = line.match(/\[(.+)\]\(([A-z0-9\.\:\@\/\-\_ㄱ-힣]+)(\s.+)?\)/);
-                    convertedHTML[id] = `<a href="${$2}"${$3?` title="${$3.replace(/[\'\"]+/gm,'').trim()}"`:''}>${$1}</a>`
+                    convertedHTML[id] = `<a href="${$2}"${$3?` title="${$3.replace(/[\'\"]+/gm,'').trim()}"`:''}>${$1}</a>`;
+                    block[id] = '';
+                }
+            });
+        }
+
+        this.paragraphs = function (){
+            block.forEach((line, id)=>{
+                if(line!=''){
+                    convertedHTML[id] = `<p>${line}</p>`;
+                    block[id] = '';
                 }
             });
         }
@@ -204,16 +287,22 @@ const Markdown = (function () {
         }
     }
     return {
-        parse(str) {
+        parse(str, options) {
             const view = new View();
             const model = new Model();
             const controller = new Controller();
 
             view.init(str);
-            model.init(view);
+            model.init(view, options);
             controller.init(model);
         }
     }
 })();
 
-Markdown.parse(test);
+Markdown.parse(test, {
+    ol: 'list-group reset',
+    ul: 'list-group reset',
+    li: 'list-item',
+    blockquote: 'blockquote blockquote-info',
+    h: true,
+});
